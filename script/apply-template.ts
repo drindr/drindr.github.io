6127080143f9626546392c9975c5db35c434d7ce
drindr <jerrychancn@outlook.com>
2025-11-19 08:49:40 +0800

    add index page

--- script/apply-template.ts
+++ script/apply-template.ts
@@ -4,7 +4,7 @@ import * as toml from "jsr:@std/toml";
 import * as fs from "jsr:@std/fs"
 import Mustache from "npm:mustache"
 
-function ws_dir(): string {
+export function ws_dir(): string {
   const script_dir = path.resolve(path.dirname(path.fromFileUrl(import.meta.url)));
   return path.dirname(script_dir)
 }
@@ -16,38 +16,43 @@ async function readGlobalMeta(): Object {
   return toml.parse(text);
 }
 
-async function applyTemplate() {
+export async function applyTemplate(html_content: string, page_meta: Object, toc: Object): string {
   const ws_dir_path = ws_dir();
-  const global_meta = await readGlobalMeta();
-  const source_path = Deno.args[0];
-  const target_path = Deno.args[1];
-  const relative_path = Deno.args[2];
-  const relative_root = "../".repeat(relative_path.split("/").length - 2);
-  const html_path = `${source_path}.html`;
-  const meta_path = `${source_path}.json`;
+  let global_meta = await readGlobalMeta();
+  global_meta.toc = toc;
+  
   const template_text = await Deno.readTextFile(`${ws_dir_path}/layout/base.html`);
-
-  const meta_mod = await import(meta_path, {
-    with: { type: "json" },
-  });
-
-  let page_meta = meta_mod.default;
-  page_meta.relative_root = relative_root;
 
   const view = {
     site: global_meta,
     page: page_meta,
-    content: await Deno.readTextFile(html_path)
+    content: html_content
   }
-  const output = Mustache.render(template_text, view);
+  return Mustache.render(template_text, view);
+}
+
+if (import.meta.main) {
+  const html_path = Deno.args[0];
+  const meta_path = Deno.args[1];
+  const toc_path = Deno.args[2];
+  const target_path = Deno.args[3];
+  const relative_path = Deno.args[4];
+  
+  const meta_mod = await import(meta_path, {
+    with: { type: "json" },
+  });
+  const toc_mod = await import(toc_path, {
+    with: { type: "json" },
+  });
+  let meta = meta_mod.default;
+  meta.relative_root = "../".repeat(relative_path.split("/").length);
+
+  const output = await applyTemplate(await Deno.readTextFile(html_path),
+    meta_mod.default, toc_mod.default);
   const target_dir = path.dirname(target_path);
   if (! await fs.exists(target_dir)) {
     await Deno.mkdir(target_dir, {recursive: true});
   }
-  console.log(output);
+  
   await Deno.writeTextFile(target_path, output);
-}
-
-if (import.meta.main) {
-  applyTemplate();
 }
--- script/generate-toc.ts
+++ script/generate-toc.ts
@@ -2,6 +2,8 @@
 
 import { walk } from "jsr:@std/fs/walk"
 import path from "node:path"
+import { applyTemplate, ws_dir } from "./apply-template.ts"
+import Mustache from "npm:mustache"
 
 function entryFromFile(content_path: string, file: Object) {
   const relative_path = path.relative(content_path, file.path);
@@ -10,18 +12,43 @@ function entryFromFile(content_path: string, file: Object) {
   const date = date_name_tags[0].split("T", 2);
   const date_str = [date[0].slice(0, 4), date[0].slice(4, 6), date[0].slice(6, 8)].join("-");
   const time_str = [date[1].slice(0,2), date[1].slice(2, 4), date[1].slice(4, 6)].join(":");
+  const dir_name = path.dirname(relative_path);
+  const ext_name = path.extname(relative_path);
+  const base_name = path.basename(relative_path, ext_name);
   return {
-    path: relative_path,
+    original_path: relative_path,
+    built_path: path.join(dir_name, base_name) + ".html",
+    path: path.join(dir_name, date_name_tags[2]) + ".html",
     name: date_name_tags[2],
     date: new Date(`${date_str}T${time_str}`),
     tags: date_name_tags[4].split("_")
   };
 }
 
+async function generateCategoryIndex(category_name: string, articles: [], toc: Object, out_path: string) {
+  const template_text = "<ul>{{ #articles }}<li><a href=\"./content/{{path}}\">{{name}}</a></li>{{ /articles }}</ul>";
+  const html_content = Mustache.render(template_text, {articles: articles});
+  for (var category of toc) {
+    if (category.name === category_name) {
+      category.current = true;
+    }
+  }
+  const output = await applyTemplate(html_content, {title: category_name, page: {relative_path: "./"}}, toc);
+  const file_name = path.join(out_path, category_name) + ".html";
+  await Deno.writeTextFile(file_name, output);
+}
+
+async function generateIndex(out_path: string, toc: Object) {
+  const index_html = await Deno.readTextFile(path.join(ws_dir(), "layout/index.html"));
+  const output = await applyTemplate(index_html, {page: {relative_path: "./"}}, toc);
+  await Deno.writeTextFile(path.join(out_path, "index.html"), output);
+}
+
 async function generateTOC() {
   const available_ext = [".md", ".org"];
   const content_path = Deno.args[0];
   const meta_out_path = Deno.args[1];
+  const out_path = Deno.args[2];
   let entries = [];
   for await (const dir_entry of Deno.readDir(content_path)) {
     if(! dir_entry.isDirectory) {
@@ -30,15 +57,23 @@ async function generateTOC() {
     let articles = [];
 
     for await (const file_entry of walk(path.join(content_path, dir_entry.name))) {
-      if(file_entry.isDirectory || file_entry.path.includes("/static/") || !available_ext.includes(path.extname(file_entry.path))) {
+      if(file_entry.isDirectory ||
+        file_entry.path.includes("/static/") ||
+        !available_ext.includes(path.extname(file_entry.path))) {
         continue;
       }
       articles.push(entryFromFile(content_path, file_entry));
     }
-    entries.push({[dir_entry.name]: articles});
+    entries.push({
+      name: dir_entry.name,
+      articles: articles});
   }
-  console.log(entries);
-  Deno.writeTextFile(meta_out_path, JSON.stringify(entries));
+  const entries_json = JSON.stringify(entries);
+  for (const category of entries) {
+    await generateCategoryIndex(category.name, category.articles, JSON.parse(entries_json), out_path);
+  }
+  await generateIndex(out_path, entries);
+  Deno.writeTextFile(meta_out_path, entries_json);
 }
 
 if (import.meta.main) {
